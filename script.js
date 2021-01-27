654295c744c74cd8e0b11dd0ff7171efc72f78d6
dylanstepa <dylan.stepa@ocsbstudent.ca>
2021-01-27 15:18:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function areaofsquare(){
+   var side = document.getElementById("side").value;
+   var output = side*side;
+   document.getElementById("area_status").innerHTML="your area is "+output;
